636b03f230e4feb1fd649c7a69cb76f953f1272c
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2025-03-03 12:38:17 +0200

    Initial commit

---
+++ Exam_CS_11-Class/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8; // для укр. тексту
+
+Console.WriteLine("------------ Welcome to Competition App ------------");
+
+Console.WriteLine("----- MENU -----");
+Console.WriteLine("1. Show all competition members");
+Console.WriteLine("2. Save data to file");
+Console.WriteLine("3. Show all competition types");
+Console.WriteLine("4. Add new competition type");
+Console.WriteLine("5. Облік результатів змагань у певному виді");
+Console.WriteLine("6. Підведення підсумків");
